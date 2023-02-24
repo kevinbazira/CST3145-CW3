a966cfe98982504dc34919318cac2a8370a629d3
kevinbazira <kvnbzr@gmail.com>
2023-02-24 10:23:46 +0400

    added service worker

---
+++ service-worker.js
@@ -0,0 +1,35 @@
+var cacheName = 'webstore';
+var cacheFiles = [
+    'index.html',
+    'webstore.webmanifest',
+    'images/icons8-grocery-store-512x512.png',
+    'images/icons8-grocery-store-192x192.png'
+    
+];
+
+// do the catching
+self.addEventListener('install', (e) => {
+    console.log('[Service Worker] Install');
+    e.waitUntil(
+        caches.open(cacheName).then((cache) => {
+            console.log('[Service Worker] Caching all the files');
+            return cache.addAll(cacheFiles);
+        })
+    );
+});
+
+// dynamic catching
+self.addEventListener('fetch', function (e) {
+    e.respondWith(
+        caches.match(e.request).then(function (r) {
+            // Download the file if it is not in the cache, 
+            return r || fetch(e.request).then(function (response) {
+                // add the new file to cache
+                return caches.open(cacheName).then(function (cache) {
+                    cache.put(e.request, response.clone());
+                    return response;
+                });
+            });
+        })
+    );
+});
